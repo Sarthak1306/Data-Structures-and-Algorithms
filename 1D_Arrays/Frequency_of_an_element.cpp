b2de29092cd46963de318008527130e618528722
Sarthak1306 <sarthak13062001@gmail.com>
2020-04-02 13:47:53 +0530

    Added 1D Arrays

---
+++ 1D_Arrays/Frequency_of_an_element.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+int main()
+{
+    int num, arr[50], number;
+    int i;
+
+    cout << "Enter number of elements in your array: ";
+    cin >> num;
+
+    cout << "Enter your array: ";
+    for (i = 0; i < num; i++)
+    {
+        cin  >> arr[i];
+    }
+
+    // Frequency is how many times a number have occurred
+    int f = 0;
+
+    // Number whose frequency has to be found
+    cout << "Enter the number whose occurence is to be found: ";
+    cin >> number;
+
+    for (i = 0; i < num; i++)
+    {
+        if (arr[i] == number)
+        {
+            f = f + 1;
+        }
+    }
+
+    cout << "Frequency = " << f;
+}
